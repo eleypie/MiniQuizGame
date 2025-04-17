33b76ae1faf787da97964e6221d416fe7b43416f
Kris-Amor <kamorstudy@gmail.com>
2025-04-17 21:10:34 +0800

    changed yung nav bar to index.html and questions

--- scripts/questions.js
+++ scripts/questions.js
@@ -2,108 +2,108 @@ const setQ1 = [
     {
     question: "When did Donald Trump begin his second term?",
     answers: ["January 20, 2025", "January 20, 2024",	"June 30, 2025",	"January 21, 2025"], 
-    correct: "January 20, 2025"
+    correct: 0
 }, 
     {
         question: "When did the Gaza Ceasefire take place?",
         answers: ["January 19-March 18, 2025", "January 19-March 18, 2024", "January 19-February 18, 2024", "February 19-March 18, 2028"], 
-        correct: "January 19-March 18, 2025"
+        correct: 0
 }, 
     {
         question: "When did the South Korean President declare Martial Law?",
         answers: ["December 3, 2024", "December 4, 2024", "December 2, 2024", "December 3, 2023"], 
-        correct: "December 3, 2024"
+        correct: 0
     
 }, 
     {
         question: "When did the South Korean President get impeached?",
         answers: ["December 13, 2024", "December 14, 2025", "December 15, 2024", "December 14, 2024"], 
-        correct: "December 14, 2024"
+        correct: 3
 }, 
     {
         question: "When did Canadian Prime Minister Mark Carney want the snap election to take place? ",
         answers: ["April 28,2024", "April 28,2025", "April 29,2025", "April 27,2025"], 
-        correct: "April 28,2025"
+        correct: 1
 }
 ]
 
 const set2 = [{
     question: "What are the dominant major and minor parties in the midterm poll elections?",
     answers: ["Nacionalista Party: dominant majority party, Lakas-CMD: dominant minority party", "Lakas-CMD: subordinate majority party, Nacionalista Party: subordinate minority party.", "Nacionalista Party: dominant majority party, Lakas-CMD: dominant minority party", "Lakas-CMD: dominant majority party, Nacionalista Party: dominant minority party."], 
-    correct: "Lakas-CMD: dominant majority party, Nacionalista Party: dominant minority party."
+    correct: 3
 }, 
     {
         question: "According to COMELEC, how many Gen Z voters will vote in the 2025 midterm elections?",
         answers: ["About 20 billion voters", "About 21 million voters", "About 19 million voters", "About 20 million voters"], 
-        correct: "About 20 million voters"
+        correct: 3
 }, 
     {
         question: "When will the 2025 Philippine Senate election take place?",
         answers: ["May 13, 2025", "June 12, 2025", "May 12, 2024", "May 12, 2025"], 
-        correct: "May 12, 2025"
+        correct: 3
 }, 
     {
         question: "What is the number of registered voters for 2025 according to COMELEC?",
         answers: ["68.5 million", "69.4 million", "68.4 million", "68.4 billion"], 
-        correct: "68.4 million"
+        correct: 2
 }, 
     {
         question: "Why do we use indelible ink, also called electoral ink, when voting?",
         answers: ["For status / posting", "To count the number of people", "To allow double voting.", "To avoid double voting."], 
-        correct: "To avoid double voting."
+        correct: 3
 }
 ]
 
 const set3 = [{
     question: "When was former President Rodrigo Duterte arrested?",
     answers: ["March 12,2025", "March 11,2024", "March 11,2025", "March 10,2025"], 
-    correct: "March 11,2025"
+    correct: 2
 }, 
     {
         question: "When was the War on Drugs in the Philippines started?",
         answers: ["June 29, 2016", "June 30, 2016", "July 1, 2016", "June 30, 2017"], 
-        correct: "June 30, 2016"
+        correct: 1
 }, 
     {
         question: "Who has Rodrigo Duterte in custody?",
         answers: ["International Criminal Court (ICC)", "Intentional Criminal Court (ICC)", "International Criminal Council (ICC)", "International College Court (ICC)"], 
-        correct: "International Criminal Court (ICC)"
+        correct: 0
 }, 
     {
         question: "What was the major charge against Rodrigo Duterte?",
         answers: ["Crimes against Humanity of corruption", "Crimes against Humanity of taking drugs", "Crimes against Humanity of kidnapping and torture", "Crimes against Humanity of murder, torture, and rape"], 
-        correct: "Crimes against Humanity of murder, torture, and rape"
+        correct: 3
 }, 
     {
         question: "Where was Rodrigo Duterte taken?",
         answers: ["The Hague in the Netherlands", "The windmill in the Netherlands", "The Hague in Russia", "The Hague with the Dutch"], 
-        correct: "The Hague in the Netherlands"
+        correct: 0
 }
 ]
 
 const set4 = [{
     question: "How big is the budget for the 2025 fiscal year in the Philippines?",
     answers: ["6.35 Trillion Pesos", "6.36 Trillion Pesos", "6.35 Billion Pesos", "7.35 Trillion Pesos"], 
-    correct: "6.35 Trillion Pesos"
+    correct: 0
 }, 
     {
         question: "What is the national headline inflation rate for March 2025?",
         answers: ["1.8 percent", "1.9 percent", "1.7 percent", "2.8 percent"], 
-        correct: "1.8 percent"
+        correct: 0
 }, 
     {
         question: "According to PSA, what is the estimated unemployment rate of the Philippines in February 2025?",
         answers: ["3.8 percent", "3.9 percent", "3.7 percent", "4.8 percent"], 
-        correct: "3.8 percent"
+        correct: 0
 }, 
     {
         question: "According to PSA, what is the projected population of the Philippines on 2055?",
         answers: ["138.67 Million", "138.69 Million", "139.67 Million", "138.67 Billion"], 
-        correct: "138.67 Million"
+        correct: 0
 }, 
     {
         question: "According to PSA, what is the full year GDP growth of the Philippines on 2055?",
         answers: ["5.6 percent", "5.7 percent",	"5.5 percent", "6.6 percent"], 
-        correct: "5.6 percent"
+        correct: 0
 }
 ]
