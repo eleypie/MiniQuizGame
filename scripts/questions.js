04509bfe4ea29feade427227a5cdae8612b49e60
Kris-Amor <kamorstudy@gmail.com>
2025-04-27 18:47:02 +0800

    Added half of the explanations

--- scripts/questions.js
+++ scripts/questions.js
@@ -2,108 +2,150 @@ const setQ1 = [
     {
     question: "When did Donald Trump begin his second term?",
     answers: ["January 20, 2025", "January 20, 2024",	"June 30, 2025",	"January 21, 2025"], 
-    correct: 0
+    correct: 0,
+    explanation: " 20th Amendment: Section 1 of the 20th Amendment, ratified in 1933, specifically sets the end of presidential terms at noon on January 20th." ,
+    resource: "https://www.bbc.com/news/articles/cde7ng85jwgo"
 }, 
     {
         question: "What happened on April 21,2025",
         answers: ["Holy Week just ended", "Pope Francis declared who the new pope is", "Pope Francis died", "A new pope got elected"], 
-        correct: 2
+        correct: 2,
+        explanation: "Cardinal Kevin Farrell, Camerlengo of the Holy Roman Church, announced the death of Pope Francis." ,
+        resource: "https://www.vaticannews.va/en/pope/news/2025-04/pope-francis-dies-on-easter-monday-aged-88.html"
 }, 
     {
         question: "Why did the South Korean President declare Martial Law?",
         answers: ["He was on a power trip", "To protect the country from 'antistate forces'", "Because he felt like it", "He was pressured to do so"], 
-        correct: 1
-    
+        correct: 1,
+        explanation: "On December 3,  President Yoon Suk Yeol declared martial law, citing the threat of North Korean communist forces and the urgent need to eliminate pro-North, anti-state elements." ,
+        resource: "https://www.asiapacific.ca/publication/explainer-yoons-failed-martial-law-decree-future-impacts"
 }, 
     {
         question: "Why did the South Korean President get impeached?",
         answers: ["Embezzelment", "Vote Frud", "Corruption", "Abuse of Power"], 
-        correct: 3
+        correct: 3,
+        explanation: "The impeachment is a response of the declaration of Martial Law by the President of South Korea, which can be considered an abuse of power." ,
+        resource: "https://blogs.loc.gov/law/2024/12/falqs-south-korean-martial-law/"
 }, 
     {
         question: "Why did Canadian Prime Minister Mark Carney want the snap election to take place? ",
         answers: ["Amassing power", "Reaction to Trump's Tariff", "Domestic Issues", "Felt like it"], 
-        correct: 1
+        correct: 1,
+        explanation: "There are several factors on why the snap election was pushed further but the main reactionary response is the tariff." ,
+        resource: "https://www.bbc.com/news/articles/crlxe4wxxj4o"
 }
 ]
 
-const setQ2 = [{
-    question: "What are the dominant major and minor parties in the midterm poll elections?",
-    answers: ["Nacionalista Party: dominant majority party, Lakas-CMD: dominant minority party", "Lakas-CMD: subordinate majority party, Nacionalista Party: subordinate minority party.", "Nacionalista Party: dominant majority party, Lakas-CMD: dominant minority party", "Lakas-CMD: dominant majority party, Nacionalista Party: dominant minority party."], 
-    correct: 3
+const setQ2 = [
+    {
+        question: "What are the dominant major and minor parties in the midterm poll elections?",
+        answers: ["Nacionalista Party: dominant majority party, Lakas-CMD: dominant minority party", "Lakas-CMD: subordinate majority party, Nacionalista Party: subordinate minority party.", "Nacionalista Party: dominant majority party, Lakas-CMD: dominant minority party", "Lakas-CMD: dominant majority party, Nacionalista Party: dominant minority party."], 
+        correct: 3,
+        explanation: "Under Resolution No. 11119, the poll body declares the dominant major and minor parties. " ,
+        resource: "https://www.pna.gov.ph/articles/1245516"
 }, 
     {
         question: "According to COMELEC, how many Gen Z voters will vote in the 2025 midterm elections?",
         answers: ["About 20 million voters", "About 21 million voters", "About 19 million voters", "About 20 billion voters"], 
-        correct: 0
+        correct: 0,
+        explanation: "Generation Z, born in 1997 to 2007, is slowly reaching the age to vote, which explains the high number of voters." ,
+        resource: "https://nowyouknowph.rappler.com/1186/gen-z-in-midterm-elections-2025/"
 }, 
     {
         question: "When will the 2025 Philippine Senate election take place?",
         answers: ["May 1, 2025", "June 12, 2025", "May 12, 2025", "May 12, 2024"], 
-        correct: 2
+        correct: 2,
+        explanation: "It is the given and assigned date for the elections." ,
+        resource: "https://www.abs-cbn.com/news/nation/2025/1/9/halalan-2025-key-dates-facts-on-philippines-midterm-elections-1611"
 }, 
     {
         question: "What is the full name of COMELEC?",
         answers: ["Cooperation on Elections", "Commerce Election", "Commission on Elections", "Commission on Electives"], 
-        correct: 2
+        correct: 2,
+        explanation: "COMELEC is the government orgnization responsible for the ballot." ,
+        resource: "https://comelec.gov.ph/?r=home"
 }, 
     {
         question: "Why do we use indelible ink, also called electoral ink, when voting?",
         answers: ["For status / posting", "To count the number of people", "To allow double voting.", "To avoid double voting."], 
-        correct: 3
+        correct: 3,
+        explanation: "Indelible ink remains in the skin for around 72 and its formula makes it hard to remove by conventional means, making it a good marker." ,
+        resource: "https://www.gmanetwork.com/news/lifestyle/content/831051/why-do-we-use-indelible-ink-during-elections/story/"
 }
 ]
 
-const setQ3 = [{
-    question: "What is former President Duterte's full name?",
-    answers: ["Digong 'Rody' Duterte", "D30 Duterte", "Rodrigo Roa Duterte", "Rodrigo Dua Duterte"], 
-    correct: 2
+const setQ3 = [
+    {
+        question: "What is former President Duterte's full name?",
+        answers: ["Digong 'Rody' Duterte", "D30 Duterte", "Rodrigo Roa Duterte", "Rodrigo Dua Duterte"], 
+        correct: 2,
+        explanation: "" ,
+        resource: ""
 }, 
     {
         question: "When was the War on Drugs in the Philippines started?",
         answers: ["June 29, 2016", "June 30, 2016", "July 1, 2016", "June 30, 2017"], 
-        correct: 1
+        correct: 1,
+        explanation: "" ,
+        resource: ""
 }, 
     {
         question: "Who has Rodrigo Duterte in custody?",
         answers: ["International Criminal Court (ICC)", "Intentional Criminal Court (ICC)", "International Criminal Council (ICC)", "International College Court (ICC)"], 
-        correct: 0
+        correct: 0,
+        explanation: "" ,
+        resource: ""
 }, 
     {
         question: "What was the major charge against Rodrigo Duterte?",
         answers: ["Crimes against Humanity of corruption", "Crimes against Humanity of taking drugs", "Crimes against Humanity of kidnapping and torture", "Crimes against Humanity of murder, torture, and rape"], 
-        correct: 3
+        correct: 3,
+        explanation: "" ,
+        resource: ""
 }, 
     {
         question: "Where was Rodrigo Duterte taken?",
         answers: ["The Hague in the Netherlands", "The windmill in the Netherlands", "The Hague in Russia", "The Hague with the Dutch"], 
-        correct: 0
+        correct: 0,
+        explanation: "" ,
+        resource: ""
 }
 ]
 
-const setQ4 = [{
-    question: "Why was the 2025 Philippine budget controversal?",
-    answers: ["Massive cuts to education, health and calamity response", "Increased Military Spending", "Pork Barrel Scam", "Corruption"], 
-    correct: 0
+const setQ4 = [
+    {
+        question: "Why was the 2025 Philippine budget controversal?",
+        answers: ["Massive cuts to education, health and calamity response", "Increased Military Spending", "Pork Barrel Scam", "Corruption"], 
+        correct: 0,
+        explanation: "" ,
+        resource: ""
 }, 
     {
         question: "What is the major cause of inflation in the Philippines?",
         answers: ["Corruption", "Tarrifs", "Wars", "Supply and Demand"], 
-        correct: 3
+        correct: 3,
+        explanation: "" ,
+        resource: ""
 }, 
     {
         question: "What is the major cause of unemployment in the Philippines?",
         answers: ["Economic instability", "Nepotism and cronyism", "Rapid population growth and insufficient job creations", "Corruption and a mismatch between skills and job requirement"], 
-        correct: 2
+        correct: 2,
+        explanation: "" ,
+        resource: ""
 }, 
     {
         question: "According to PSA, what is the projected population of the Philippines on 2055?",
         answers: ["138.67 Million", "138.69 Million", "139.67 Million", "138.67 Billion"], 
-        correct: 0
+        correct: 0,
+        explanation: "" ,
+        resource: ""
 }, 
     {
         question: "What is the Philippines' rank in the GDP index?",
-        answers: ["1", "33",	"75", "181"], 
-        correct: 1
+        answers: ["1", "33", "75", "181"], 
+        correct: 1,
+        explanation: "" ,
+        resource: ""
 }
 ]
